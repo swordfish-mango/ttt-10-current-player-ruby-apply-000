9928763643b6c3617a151229b27ec3e3c6301c42
Marcus Shimotsu <marcus.shimotsu@gmail.com>
2021-01-02 23:55:27 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,4 +1,15 @@
 def current_player(board)
   if turn_count(board) % 2 == 0
-    
+    "0"
+  else
+    "X"
+  end
+end
+
+def turn_count(board)
+  count = 0
+  board.each do |element|
+  if element == "X" or element == "O"
+     count += 1
+  end
 end
